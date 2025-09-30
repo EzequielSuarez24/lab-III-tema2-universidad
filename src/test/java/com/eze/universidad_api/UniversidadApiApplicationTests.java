065e6c25fc987e897e458f82296f48091b9ba3a8
Ezequiel Suarez <ezegsuarez@gmail.com>
2025-09-29 21:48:25 -0300

    Proyecto de arranque de Spring Bootstrap con H2 y Jacobo

---
+++ src/test/java/com/eze/universidad_api/UniversidadApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eze.universidad_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversidadApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
